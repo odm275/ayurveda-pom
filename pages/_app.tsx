e55665cc33081c3c44b912737cb9bf8a3c8a31fb
odm275 <pomejia@gmail.com>
2021-06-10 19:27:24 -0500

    adding css globally for now just to get it working and seeing our line graph show up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ProvideAuth } from '../lib/context/AuthContext';
 import { useApollo } from '../apollo/client';
 
+import '../styles/styles.css';
+import '../styles/chart.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
