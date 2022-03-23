282945594aa33f4394355600d0b79c5265227e54
odm275 <pomejia@gmail.com>
2022-03-22 21:26:15 -0500

    Add Logo

--- lib/components/Navbar/components/MainMenu/MainMenu.tsx
+++ lib/components/Navbar/components/MainMenu/MainMenu.tsx
@@ -1,20 +1,26 @@
+import NextLink from "next/link";
 import {
   Flex,
-  Text,
+  Icon,
+  Box,
   useBreakpointValue,
-  useColorModeValue
+  useColorModeValue,
+  Link,
+  Text
 } from "@chakra-ui/react";
 import { DesktopNav } from "./components";
-
+import WindLogo from "../../../../../public/wind.svg";
 export const MainMenu = () => (
-  <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-    <Text
-      textAlign={useBreakpointValue({ base: "center", md: "left" })}
-      fontFamily={"heading"}
-      color={useColorModeValue("gray.800", "white")}
-    >
-      Logo
-    </Text>
+  <Flex
+    flex={{ base: 1 }}
+    justify={{ base: "center", md: "start" }}
+    alignItems="center"
+  >
+    <Link textAlign={useBreakpointValue({ base: "center", md: "left" })}>
+      <NextLink href="/">
+        <Icon as={WindLogo} w={12} h={12} fill="pink.400" />
+      </NextLink>
+    </Link>
 
     <Flex display={{ base: "none", md: "flex" }} ml={10}>
       <DesktopNav />
