3d6a945468cd79a417920c9514e5d76a77ec16ff
odm275 <pomejia@gmail.com>
2022-03-29 17:09:38 -0500

    we will keep Line as jsx for now

--- lib/components/Chart/Line.jsx
+++ lib/components/Chart/Line.jsx
@@ -1,15 +1,6 @@
 import React, { ReactElement } from "react";
 import * as d3 from "d3";
 
-interface Props {
-  type?: "line" | "area";
-  data: any;
-  xAccessor: (any) => number;
-  yAccessor: (any) => number;
-  y0Accessor?: any;
-  interpolation?: d3.CurveFactory;
-}
-
 export const Line = ({
   type = "line",
   data,
@@ -18,11 +9,14 @@ export const Line = ({
   y0Accessor = "0",
   interpolation = d3.curveMonotoneX,
   ...props
-}: Props): ReactElement => {
-  const lineGenerator = d3[type](xAccessor, yAccessor).curve(interpolation);
+}) => {
+  const lineGenerator = d3[type]()
+    .x(xAccessor)
+    .y(yAccessor)
+    .curve(interpolation);
 
   if (type == "area") {
-    // lineGenerator.y0(y0Accessor).y1(yAccessor);
+    lineGenerator.y0(y0Accessor).y1(yAccessor);
   }
 
   const line = lineGenerator(data);
