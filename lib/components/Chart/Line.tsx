f563b5939016bec29b3087c2d1ec2c7d86f41259
odm275 <pomejia@gmail.com>
2022-03-29 17:14:10 -0500

    experimenting to see if it will compile

--- lib/components/Chart/Line.tsx
+++ lib/components/Chart/Line.tsx
@@ -1,6 +1,15 @@
 import React, { ReactElement } from "react";
 import * as d3 from "d3";
 
+interface Props {
+  type?: "line" | "area";
+  data: any;
+  xAccessor: (any) => number;
+  yAccessor: (any) => number;
+  y0Accessor?: any;
+  interpolation?: d3.CurveFactory;
+}
+
 export const Line = ({
   type = "line",
   data,
@@ -9,7 +18,7 @@ export const Line = ({
   y0Accessor = "0",
   interpolation = d3.curveMonotoneX,
   ...props
-}) => {
+}: Props): ReactElement => {
   const lineGenerator = d3[type]()
     .x(xAccessor)
     .y(yAccessor)
